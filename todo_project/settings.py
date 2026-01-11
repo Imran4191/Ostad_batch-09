579a044a06e523a4a44c890f3acc0356d6a3f935
Md.Latifur Rahman Rafi <133046039+latifurrafi@users.noreply.github.com>
2026-01-11 23:05:37 +0600

    Add .onrender.com to CSRF trusted origins

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = [
     '.ngrok.io',  # Allow all ngrok.io subdomains (for paid ngrok)
     'testtttttt-kf9v.onrender.com',
     'ostad-batch-09-1-cwz9.onrender.com',
+    '.onrender.com',
+
 ]
 
 # CSRF trusted origins for ngrok
